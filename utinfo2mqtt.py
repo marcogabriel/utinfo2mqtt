02aa639104015cabb55ee507f3a06d1e1926b47b
Marco Gabriel <marcomgabriel@gmail.com>
2020-01-23 17:04:09 +0000

    added influxdb config options and cost options

--- utinfo2mqtt.py
+++ utinfo2mqtt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # utinfo2mqtt.py (c) 01/2020 Marco Gabriel <mail@marcogabriel.com>
@@ -12,6 +12,9 @@ from pprint import pprint
 from time import time
 import paho.mqtt.client as mqtt
 from configparser import ConfigParser
+from influxdb import InfluxDBClient
+import datetime
+import time
 
 
 class Linky(object):
@@ -25,12 +28,17 @@ class Linky(object):
 
     def run(self, config):
         self.config = config
-        self.connect_mqtt()
+        #self.connect_mqtt()
+        self.connect_influxdb()
         for frame in Parser(UTInfo2()):
             print(frame)
-            for item in frame:
-                pprint(item + ":" + frame[item])
-                self.client.publish("linky/"+ item, frame[item])
+            self.send_to_influx(frame)
+            #for item in frame:
+            #    pprint(item + ":" + frame[item])
+            #    self.client.publish("linky/"+ item, frame[item])
+
+    def import_from_file(self, config):
+        self.config = config
 
     def connect_mqtt(self):
         self.client = mqtt.Client()
@@ -39,6 +47,43 @@ class Linky(object):
         self.client.connect(self.config["mqtt"]["mqtt_host"], 1883, 60)
         self.client.loop_start()
 
+    def connect_influxdb(self):
+        client = InfluxDBClient(
+                self.config['influxdb']['host'], 
+                self.config['influxdb']['port'], 
+                self.config['influxdb']['username'], 
+                self.config['influxdb']['password'], 
+                self.config['influxdb']['database'])
+        self.influx = client
+
+    def send_to_influx(self, frame):
+        # type conversion for influx
+        frame['HCHC'] = float(frame['HCHC'])
+        frame['HCHP'] = float(frame['HCHP'])
+        frame['PAPP'] = float(frame['PAPP'])
+        frame['PMAX'] = float(frame['PMAX'])
+        frame['IINST1'] = float(frame['IINST1'])
+        frame['IINST2'] = float(frame['IINST2'])
+        frame['IINST3'] = float(frame['IINST3'])
+        frame['IMAX1'] = float(frame['IMAX1'])
+        frame['IMAX2'] = float(frame['IMAX2'])
+        frame['IMAX3'] = float(frame['IMAX3'])
+        # create timestamp for influx
+        dt = datetime.datetime.now()
+        timestamp = dt.isoformat("T")
+        # create frame for influx
+        json_body = [{
+            "measurement": "linky_frame",
+            "tags": {
+                "powermeter": "linky_1"
+            },
+            "time": timestamp,
+            "fields": frame
+        }]
+        self.influx.create_database("linky")
+        self.influx.write_points(json_body)
+
+
     #def on_connect(self, self.client, userdata, flags, rc):
     #    print("Connected with result code {}".format(rc))
 
@@ -46,6 +91,8 @@ class Linky(object):
 if __name__ == '__main__':
     t = Linky()
     config = ConfigParser()
-    print(config.read("utinfo2mqtt.conf"))
+    #print(
+    config.read("utinfo2mqtt.conf")
+    #)
     t.run(config)
 
