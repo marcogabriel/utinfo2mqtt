ac4a962404ee633e927e75ebbccde5302f2fa827
Marco Gabriel <mgabriel@inett.de>
2020-06-15 11:53:17 +0200

    daten werden jetzt an mqtt und influx gesendet. nur der erste datensatz nach start wird komplett gesendet, danach nur noch änderungen.

--- utinfo2mqtt.py
+++ utinfo2mqtt.py
@@ -26,16 +26,29 @@ class Linky(object):
         ti = Teleinfo(RpiDom())
         print(ti.get_frame())
 
+    def changed_items(self, frame):
+        return_items = dict()
+        for item in frame:
+            if frame[item] == self.lastframe.get(item):
+                #print("{} --- {}".format(frame[item], self.lastframe.get(item, frame[item])))
+                continue
+            return_items[item] = frame[item]
+        self.lastframe = frame
+        return return_items
+
     def run(self, config):
         self.config = config
         self.connect_mqtt()
         self.connect_influxdb()
+        self.lastframe = dict()
         for frame in Parser(UTInfo2()):
-            print(frame)
-            self.send_to_influx(frame)
-            for item in frame:
-                #print("{}:{}".format(item, frame[item]))
-                self.client.publish("linky/"+ item, frame[item])
+            frame = self.changed_items(frame)
+            if len(frame.keys()) > 0:
+                print(frame)
+                self.send_to_influx(frame)
+                for item in frame:
+                    #print("{}:{}".format(item, frame[item]))
+                    self.client.publish("linky/"+ item, frame[item])
 
     def import_from_file(self, config):
         self.config = config
@@ -58,16 +71,27 @@ class Linky(object):
 
     def send_to_influx(self, frame):
         # type conversion for influx
-        frame['HCHC'] = float(frame['HCHC'])
-        frame['HCHP'] = float(frame['HCHP'])
-        frame['PAPP'] = float(frame['PAPP'])
-        frame['PMAX'] = float(frame['PMAX'])
-        frame['IINST1'] = float(frame['IINST1'])
-        frame['IINST2'] = float(frame['IINST2'])
-        frame['IINST3'] = float(frame['IINST3'])
-        frame['IMAX1'] = float(frame['IMAX1'])
-        frame['IMAX2'] = float(frame['IMAX2'])
-        frame['IMAX3'] = float(frame['IMAX3'])
+        if frame.has_key("HCHC"):
+            frame['HCHC'] = float(frame['HCHC'])
+        if frame.has_key("HCHP"):
+            frame['HCHP'] = float(frame['HCHP'])
+        if frame.has_key("PAPP"):
+            frame['PAPP'] = float(frame['PAPP'])
+        if frame.has_key("PMAS"):
+            frame['PMAX'] = float(frame['PMAX'])
+        if frame.has_key("IINST1"):
+            frame['IINST1'] = float(frame['IINST1'])
+        if frame.has_key("IINST2"):
+            frame['IINST2'] = float(frame['IINST2'])
+        if frame.has_key("IINST3"):
+            frame['IINST3'] = float(frame['IINST3'])
+        if frame.has_key("IMAX1"):
+            frame['IMAX1'] = float(frame['IMAX1'])
+        if frame.has_key("IMAX2"):
+            frame['IMAX2'] = float(frame['IMAX2'])
+        if frame.has_key("IMAX3"):
+            frame['IMAX3'] = float(frame['IMAX3'])
+
         # create timestamp for influx
         dt = datetime.datetime.now()
         timestamp = dt.isoformat("T")
